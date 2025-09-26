39524bbd9e6751f588a1d1e657dd81d18957a8fb
IvanSlivenko <ivan8822@ukr.net>
2025-09-26 13:51:58 +0300

    26-09-2025-13-51

--- node-beginer/01-first-server/index.js
+++ node-beginer/01-first-server/index.js
@@ -1,5 +1,19 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
+    response.write('<h1>Hello node !</h1>');
+    response.end();
+});
 
-})
+console.log('server starting')
+server.listen(80)
+
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+//     res.end('<h1>Hello Node!</h1>');
+// });
+
+// console.log('Server starting on http://localhost:3000');
+// server.listen(3000);
